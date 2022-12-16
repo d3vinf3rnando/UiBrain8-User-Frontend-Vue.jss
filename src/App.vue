90dc0b33e25ccc03810225f09b95855ec22e4071
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2022-12-16 14:12:39 +0530

    landing page

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,26 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
-
 </script>
 
 <template>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-  <div>
-    <p class="is-size-7">Hello world</p>
+  <div class="has-text-left">
+    <h2 class="has-text-left has-size-1"> UiBrain8</h2>
+
+    
     
   </div>
+
   
 </template>
 
 <style scoped>
 
-header {
-  line-height: 1.5;
+
+
+h2{
+  color:blue;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-
 
 </style>
